36077f3e422e0eb9e1fe845149d308e2b287f5ad
Guilherme Luiz T. Gonzaga <104567893+GuilhermeoLuiz@users.noreply.github.com>
2023-08-11 16:16:06 -0300

    Concertando erro

--- template-galeria.php
+++ template-galeria.php
@@ -4,15 +4,12 @@
  */
 get_header(); ?>
 
-
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/gallerystyle.css">
-    <main>
-        <?php 
-        $folder = $_POST["pasta"];
-        if($folder == NULL){
-            $folder = "principal";
-        }
-        gallery($folder);?>
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/gallerystyle.css">
+<main>
+    <?php
+    $folder = isset($_POST["pasta"]) ? $_POST["pasta"] : "principal";
+    gallery($folder);
+    ?>
 
     <?php
     $temaDir = get_template_directory(); // Diretório do tema
@@ -21,33 +18,36 @@ get_header(); ?>
     $conteudoTema = scandir($temaDir);
 
     // Filtrar apenas as pastas (diretórios) que não sejam ocultas
-    $pastas = array_filter($conteudoTema, function($item) use ($temaDir) {
+    $pastas = array_filter($conteudoTema, function ($item) use ($temaDir) {
         return is_dir($temaDir . '/' . $item) && $item !== '.' && $item !== '..' && strpos($item, '.') !== 0;
     });
+
     echo "<div><h1> Galerias </h1></div>";
-    echo "<ul>"; 
+    echo "<ul>";
     foreach ($pastas as $pasta) {
         ?>
-        <form id="forms" action="<?php get_template_directory_uri();?>" method="POST">
-            <input type="submit" name="pasta" class="myButton" value="<?php echo esc_html($pasta)?>">
+        <form id="forms" action="<?php echo esc_url(admin_url('admin-post.php')); ?>" method="POST">
+            <input type="hidden" name="action" value="process_gallery">
+            <input type="hidden" name="pasta" value="<?php echo esc_attr($pasta); ?>">
+            <input type="submit" name="submit_button" class="myButton" value="<?php echo esc_html($pasta); ?>">
         </form>
         <?php
     }
     echo "</ul>";
     ?>
+
     <?php
     if (is_user_logged_in()) {
         // O usuário está logado
-        ?> 
-    <a href="<?php echo esc_url(get_stylesheet_directory_uri() . '/novagaleria.php')?>"> Adicionar nova Galeria</a>
-    <a href="<?php echo esc_url(get_stylesheet_directory_uri() . '/removergaleria.php')?>"> Remover Galeria</a>
-    <?php 
-    }?>
-    </main>
-    <script src="<?php echo get_template_directory_uri(); ?>/gallery.js"></script>
-    
-
+        ?>
+        <a href="<?php echo esc_url(get_stylesheet_directory_uri() . '/novagaleria.php')?>"> Adicionar nova Galeria</a>
+        <a href="<?php echo esc_url(get_stylesheet_directory_uri() . '/removergaleria.php')?>"> Remover Galeria</a>
+        <?php
+    }
+    ?>
+</main>
+<script src="<?php echo get_template_directory_uri(); ?>/gallery.js"></script>
 
 <?php get_sidebar(); ?>
+<?php get_footer(); ?>
 
-<?php get_footer();
