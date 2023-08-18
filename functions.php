0fc25088ab4884355cdd255b65630155811162e7
Guilherme Luiz T. Gonzaga <104567893+GuilhermeoLuiz@users.noreply.github.com>
2023-08-18 15:15:10 -0300

    Add files via upload

--- functions.php
+++ functions.php
@@ -736,15 +736,72 @@ add_action('save_post_curso', 'save_curso_metabox');
 
 // Exibir Cursos por Tipo
 function display_cursos() {
-    $tipos_cursos = array('graduacao', 'posgraduacao', 'extensao', 'pesquisa');
+    echo '<h1 class="titulo">Cursos</h1><br>';
+    $tipos_cursos = array(
+        'graduacao' => 'Graduação',
+        'posgraduacao' => 'Pós-graduação',
+        'extensao' => 'Extensão',
+        'pesquisa' => 'Pesquisa'
+    );
 
-    foreach ($tipos_cursos as $tipo) {
-        echo '<h1 class="titulo">' . ucfirst($tipo) . 's</h1><br>';
+    foreach ($tipos_cursos as $tipo => $tipo_display) {
+        echo '<h2 class="titulo">' . $tipo_display . '</h2><br>';
         $args = array(
             'post_type' => 'curso',
             'posts_per_page' => -1,
-            'meta_key' => 'curso_tipo',
-            'meta_value' => $tipo,
+            'meta_query' => array(
+                array(
+                    'key' => 'curso_tipo',
+                    'value' => $tipo,
+                    'compare' => '='
+                )
+            )
+        );
+        $query = new WP_Query($args);
+
+        if ($query->have_posts()) {
+            echo '<div class="curso-section">';
+            echo '<ul class="curso-list">';
+            while ($query->have_posts()) {
+                $query->the_post();
+                echo '<li class="curso-item">';
+                echo '<h2>' . get_the_title() . '</h2>';
+
+                if (has_post_thumbnail()) {
+                    the_post_thumbnail('thumbnail', array('class' => 'curso-image'));
+                }
+            }
+            echo '</ul>';
+            echo '</div>';
+            wp_reset_postdata();
+        } else {
+            echo '<p>Nenhum curso de ' . $tipo_display . ' disponível.</p>';
+        }
+    }
+}
+
+// Exibir Cursos por Tipo e com conteúdo
+function display_cursos2() {
+    echo '<h1 class="titulo">Cursos</h1><br>';
+    $tipos_cursos = array(
+        'graduacao' => 'Graduação',
+        'posgraduacao' => 'Pós-graduação',
+        'extensao' => 'Extensão',
+        'pesquisa' => 'Pesquisa'
+    );
+
+    foreach ($tipos_cursos as $tipo => $tipo_display) {
+        echo '<h1 class="titulo">' . $tipo_display . '</h1><br>';
+        $args = array(
+            'post_type' => 'curso',
+            'posts_per_page' => -1,
+            'meta_query' => array(
+                array(
+                    'key' => 'curso_tipo',
+                    'value' => $tipo,
+                    'compare' => '='
+                )
+            )
         );
         $query = new WP_Query($args);
 
@@ -767,7 +824,7 @@ function display_cursos() {
             echo '</div>';
             wp_reset_postdata();
         } else {
-            echo '<p>Nenhum curso de ' . $tipo . ' disponível.</p>';
+            echo '<p>Nenhum curso de ' . $tipo_display . ' disponível.</p>';
         }
     }
 }
