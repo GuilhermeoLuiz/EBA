4379d4f835499783510fa5e0c462d89a3854e8a1
Thales <2021951469@teiacoltec.org>
2023-08-28 13:50:26 -0300

    28/08

--- gallery2.js
+++ gallery2.js
@@ -1,28 +1,14 @@
-const gallery = document.querySelector('.gallery');
-const slides = document.querySelectorAll('.slide');
-let currentIndex = 0;
+document.addEventListener('DOMContentLoaded', function() {
+    const gallery = document.querySelector('.gallery');
+    let rotation = 0;
 
-document.addEventListener('keydown', handleKeydown);
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'ArrowLeft') {
+            rotation += 1;
+        } else if (event.key === 'ArrowRight') {
+            rotation -= 1;
+        }
 
-function handleKeydown(event) {
-    if (event.key === 'ArrowLeft') {
-        showSlide(currentIndex - 1);
-    } else if (event.key === 'ArrowRight') {
-        showSlide(currentIndex + 1);
-    }
-}
-
-function showSlide(index) {
-    if (index < 0) {
-        index = slides.length - 1;
-    } else if (index >= slides.length) {
-        index = 0;
-    }
-    
-    slides[currentIndex].classList.remove('active');
-    slides[index].classList.add('active');
-    
-    currentIndex = index;
-}
-
-showSlide(currentIndex);  // Show the initial slide
+        gallery.style.transform = `rotateY(${rotation}deg)`;
+    });
+});
