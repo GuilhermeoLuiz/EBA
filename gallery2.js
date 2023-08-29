d1b647915a2fb8884151bccc4ff3f4a41f54d0fb
Θαλησ <104565512+Thalesed@users.noreply.github.com>
2023-08-29 13:21:19 -0300

    Add files via upload

--- gallery2.js
+++ gallery2.js
@@ -1,14 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const gallery = document.querySelector('.gallery');
-    let rotation = 0;
+const gallery = document.querySelector('.gallery');
+const slides = document.querySelectorAll('.slide');
+let currentIndex = 0;
 
-    document.addEventListener('keydown', function(event) {
-        if (event.key === 'ArrowLeft') {
-            rotation += 1;
-        } else if (event.key === 'ArrowRight') {
-            rotation -= 1;
-        }
+document.addEventListener('keydown', handleKeydown);
 
-        gallery.style.transform = `rotateY(${rotation}deg)`;
-    });
-});
+function handleKeydown(event) {
+    if (event.key === 'ArrowLeft') {
+        showSlide(currentIndex - 1);
+    } else if (event.key === 'ArrowRight') {
+        showSlide(currentIndex + 1);
+    }
+}
+
+function showSlide(index) {
+    if (index < 0) {
+        index = slides.length - 1;
+    } else if (index >= slides.length) {
+        index = 0;
+    }
+    
+    slides[currentIndex].classList.remove('active');
+    slides[index].classList.add('active');
+    
+    currentIndex = index;
+}
+
+showSlide(currentIndex);  // Show the initial slide
