fd5945e37c4236004931f893246a14e02ca167ee
Thales <2021951469@teiacoltec.org>
2023-08-29 13:38:47 -0300

    29/08

--- gallery2.js
+++ gallery2.js
@@ -1,28 +1,19 @@
-const gallery = document.querySelector('.gallery');
-const slides = document.querySelectorAll('.slide');
-let currentIndex = 0;
+document.addEventListener('DOMContentLoaded', function() {
+    const gallery = document.querySelector('.gallery');
+    let rotation = 0;
 
-document.addEventListener('keydown', handleKeydown);
+    const rotateGallery = () => {
+        gallery.style.transition = 'transform 0.5s ease-in-out';
+        gallery.style.transform = `translateX(200px) rotateY(${rotation}deg)`; // Mantém o translateX
+    };
 
-function handleKeydown(event) {
-    if (event.key === 'ArrowLeft') {
-        showSlide(currentIndex - 1);
-    } else if (event.key === 'ArrowRight') {
-        showSlide(currentIndex + 1);
-    }
-}
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'ArrowLeft') {
+            rotation += 60;
+        } else if (event.key === 'ArrowRight') {
+            rotation -= 60;
+        }
 
-function showSlide(index) {
-    if (index < 0) {
-        index = slides.length - 1;
-    } else if (index >= slides.length) {
-        index = 0;
-    }
-    
-    slides[currentIndex].classList.remove('active');
-    slides[index].classList.add('active');
-    
-    currentIndex = index;
-}
-
-showSlide(currentIndex);  // Show the initial slide
+        rotateGallery();
+    });
+});
