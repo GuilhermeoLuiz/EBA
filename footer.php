64dc4c51bf6fed1d07e515cc4af6b9a9055f9bfe
Guilherme Luiz T. Gonzaga <104567893+GuilhermeoLuiz@users.noreply.github.com>
2023-09-01 16:47:32 -0300

    Corrigindo paleta de cores

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
 <footer>
         <?php if (is_active_sidebar('footer')) : ?>
 		<div class="widget-area">
+                        Feito por Guilherme e Thales
                 	<?php dynamic_sidebar('footer'); ?>
                 </div>
         <?php endif; ?>
