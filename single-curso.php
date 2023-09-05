016dfe1d460edcc1bbdaed46088690f184bfc90a
Θαλησ <104565512+Thalesed@users.noreply.github.com>
2023-09-05 16:21:41 -0300

    Add files via upload

--- single-curso.php
+++ single-curso.php
@@ -3,9 +3,15 @@
 get_header();
 ?>
 <main>
+<div class="cursos-image"> 
 <?php
+if (has_post_thumbnail()) {
+    the_post_thumbnail('thumbnail', array('class' => 'curso-image'));
+}
 ?>
+</div>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
 <h2><?php the_title(); ?></h2>
 <div class="entry-meta">
     <p>Data de Publicação: <?php echo date_i18n('j \d\e F \d\e Y', strtotime(get_the_date())); ?></p>
