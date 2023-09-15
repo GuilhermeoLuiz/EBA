55f2f2e6d96ac607e8a6468af02a97b6c6177b96
Thales <2021951469@teiacoltec.org>
2023-09-15 15:56:40 -0300

    15/09

--- search.js
+++ search.js
@@ -1,9 +1,13 @@
 var lupa = document.querySelector('.lupa');
 var form = document.getElementById("searchform");
+var title = document.querySelector('.texto');
 
 lupa.addEventListener('click', function(event) {
     console.log("teste32");
     form.style.display = "flex";
+    if (window.innerWidth < 768) {
+        title.style.display = 'none';
+    }
     event.stopPropagation(); // Impede a propagação do clique para a tela
 });
 
@@ -13,6 +17,7 @@ form.addEventListener('click', function(event) {
 
 document.addEventListener('click', function(event) {
     if (event.target !== lupa && event.target !== form) {
+        title.style.display = 'initial';
         form.style.display = "none";
     }
 });
